b74bc0b7a612f54330e005699f1e3b2767ec829f
Vishant <vishantk@gmail.com>
2018-01-11 14:24:25 -0800

    crawler test commit

---
+++ src/SpiderTest.java
@@ -0,0 +1,16 @@
+public class SpiderTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+    	  int x = 7 / 3;
+    	  System.out.print(x);
+//        Spider spider = new Spider();
+//        spider.search("http://google.com/", "z9m3f");
+    }
+}
